24a093cf657c0376cecc52188ffb561820e470b3
Kosy Anyanwu <kosy.anyanwu@andela.com>
2016-03-14 10:58:18 +0100

    Update variables, accept input arguments for filepath

--- devopstest.sh
+++ devopstest.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 echo "This is my first devops script"
 
-FILE_PATH=/Users/kosyanyanwu/devopsshared
+FILE_PATH=$1
 
-echo "Currently in " $FILE_PATH
+if [ -z "${FILE_PATH}" ]
+then
+    echo "No argument supplied. Expected a file path"
+    echo "Creating directory in current path"
+    mkdir devopsshared && cd devopsshared
+    FILE_PATH=$(pwd)
+fi
+
+echo "Currently in " ${FILE_PATH}
 
 echo "Creating 30 subdirectories with 5 files in devopsshared"
 for i in {1..30}
 do
-    mkdir $FILE_PATH/devopsshared_$i
+    mkdir ${FILE_PATH}/devopsshared_$i
     for j in {1..5}
     do
-        touch $FILE_PATH/devopsshared_$i/devopsshared_$i_file$j.txt
+        touch ${FILE_PATH}/devopsshared_$i/devopsshared_$i_file$j.txt
     done
 done
 
 echo "Creating devopsshared_backup folder in devopsshared"
-mkdir $FILE_PATH/devopsshared_backup
+mkdir ${FILE_PATH}/devopsshared_backup
--- devopstest_backup.sh
+++ devopstest_backup.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 echo "Running devopstest backup script"
 
-FILE_PATH=/Users/kosyanyanwu/devopsshared
+FILE_PATH=$1
 
-echo "Currently in " $FILE_PATH
+if [ -z "${FILE_PATH}" ]
+then
+    echo "No argument supplied. Expected file path to devopsshared directory"
+    exit 1
+fi
+
+echo "Currently in " ${FILE_PATH}
+
 echo "Zipping devopsshared_0*_file1.txt, file3.txt and file5.txt"
 
 for i in {1..30}
 do
-    zip devopsshared_$i.zip $FILE_PATH/devopsshared_$i/devopsshared_[135].txt
-    mv devopsshared_$i.zip $FILE_PATH/devopsshared_backup
+    zip devopsshared_$i.zip ${FILE_PATH}/devopsshared_$i/devopsshared_[135].txt
+    mv devopsshared_$i.zip ${FILE_PATH}/devopsshared_backup
 done
 
 echo "Zipping devopsshared_backup"
 CURRENT_DATE=$(date "+%Y-%m-%d")
-zip $FILE_PATH/devopsshared_backup_$CURRENT_DATE  $FILE_PATH/devopsshared_backup
+zip ${FILE_PATH}/devopsshared_backup_$CURRENT_DATE  ${FILE_PATH}/devopsshared_backup
